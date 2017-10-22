cf794b0c0819e6df8c653bbbcece2ccdc604aa88
Kevin <paledust@hotmail.com>
2017-10-22 16:00:38 -0400

    Text Mat Experiment

---
+++ MIRROR_TEXT/Assets/Script/TypeIn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TypeIn : MonoBehaviour {
+	[SerializeField] InputField inputField;
+	[SerializeField] DisplayText display;
+	// Use this for initialization
+	void Start () {
+		inputField.onEndEdit.AddListener(AcceptStringInput);
+	}
+	void AcceptStringInput(string userInput){
+		userInput = userInput.ToLower();
+		if(userInput == "reset"){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+		int method = Random.Range(0,4);
+		switch (method)
+		{
+			case 0:
+				display.LogInText(userInput,2);
+				break;
+			case 1:
+				display.LogInText(userInput,0);
+				break;
+			case 2:
+				display.LogInText_Line(userInput,3);
+				break;
+			case 3:
+				display.LogInText_Space(userInput,3);
+				break;
+		}
+
+		InputComplete();
+	}
+	void InputComplete(){
+		display.DisplayLoggedText ();
+        inputField.ActivateInputField ();
+        inputField.text = null;
+	}
+}
---
+++ MIRROR_TEXT/Assets/Script/Util/Event.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cumstom_Event
+{
+	public abstract class Event {
+		public delegate void Handler(Event e);
+	}
+	public class typeEnter_Event: Event{
+
+	}
+}
